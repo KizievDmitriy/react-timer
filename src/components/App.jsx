f15d035d743c36b5b2856e67e32736e65c444781
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-07 17:49:17 +0300

    add timer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import Timer from "./Timer";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <Timer/>
   );
 };
